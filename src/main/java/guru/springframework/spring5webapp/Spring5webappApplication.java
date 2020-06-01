a345f14a7183c88b475366d65fb9e500bcf57a24
Progrema Studio <iqbalpakeh@gmail.com>
2020-06-02 06:18:54 +0700

    Add first two pojos

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5webappApplication {
 
-	// Add my comment here!
-
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
