43402a5d3dfa0f94e8632d1d184b4c078cda7348
Progrema Studio <iqbalpakeh@gmail.com>
2020-06-03 05:00:11 +0700

    Implement repository and BootstrapData

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -25,6 +26,7 @@ public class Book {
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+        this.authors = new HashSet<>();
     }
 
     public Long getId() {
